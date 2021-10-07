6a49e5a5c80122e63e35b9a5634146de78a73be4
MingZi <1127abj@gmail.com>
2021-10-07 15:53:01 +0900

    second commit

--- C00.c
+++ C00.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
 #include <stdio.h>
 
+// 00
 void ft_putchar(char c)
 {
     write(1, &c, 1);
 }
 
+// 01
 void ft_print_alphabet(void)
 {
     char c;
@@ -17,6 +19,7 @@ void ft_print_alphabet(void)
     }
 }
 
+// 02
 void ft_print_reverse_alphabet(void)
 {
     char c;
@@ -28,6 +31,31 @@ void ft_print_reverse_alphabet(void)
     }
 }
 
+// 03
+void ft_print_numbers(void)
+{
+    int n = 0;
+    while (n < 10)
+    {
+        ft_putchar('0' + n);
+        n++;
+    }
+}
+
+// 04
+void ft_is_negative(int n)
+{
+    if (n < 0)
+    {
+        ft_putchar('N');
+    }
+    else
+    {
+        ft_putchar('P');
+    }
+}
+
+// for 05 ~ 08
 int ft_check_end(char *arr, int size)
 {
     int index = size - 1;
@@ -51,24 +79,86 @@ int ft_check_end(char *arr, int size)
     }
 }
 
+void print_num(char *arr, int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        ft_putchar(arr[i]);
+    }
+    if(ft_check_end(arr, size) == 1)
+    {
+        return;
+    }
+    else
+    {
+        ft_putchar(',');
+        ft_putchar(' ');
+        return;
+    }
+}
+
+// 05
+void ft_print_comb(void)
+{
+    // start num
+    int index = 0;
+    char arr[3] = {'0','1','1'};
+
+    while(ft_check_end(arr, 3) != 1)
+    {
+        int i = 1;
+        int check_size = 0;
+
+        // last number 9 -> change number
+        while(arr[2] == '9' - i + 1)
+        {
+            check_size++;
+            i++;
+        }
+        
+        // from back -> change number
+        if(check_size > 0)
+        {
+            arr[2 - check_size ]++;
+            while(check_size > 0)
+            {
+                arr[3 - check_size] = arr[2 - check_size] + 1;
+                check_size--;
+            }
+        }
+        else
+        {
+            arr[2]++;
+        }
+        print_num(arr, 3);
+    }
+    return;
+}
+
+// 06
+void ft_print_comb2(void)
+{
+    int index = 0;
+    char arr1[2] = {'0', '0'};
+    char arr2[2] = {'0', '0'};
+
+    
+}
+
+
+// 08
 void ft_print_combn(int n)
 {
     int index = 0;
     char arr[n];
 
-    // reset to '0123456789'
+    // reset to '0123456788'
     while(index < n)
     {
         arr[index] = '0' + index;
         index++;
     }
-
-    for(int i = 0; i < n; i++)
-    {
-        ft_putchar(arr[i]);
-    }
-    ft_putchar(',');
-    ft_putchar(' ');
+    arr[index]--;
 
     // while not end -> keep plus
     while(ft_check_end(arr, n) != 1)
@@ -79,8 +169,8 @@ void ft_print_combn(int n)
         // last number 9 -> change number
         while(arr[n - i] == '9' - i + 1)
         {
-        check_size++;
-        i++;
+            check_size++;
+            i++;
         }
 
         // from back -> change number
@@ -89,7 +179,7 @@ void ft_print_combn(int n)
             arr[n - 1 - check_size ]++;
             while(check_size > 0)
             {
-                arr[n - check_size] = arr[n - check_size - 1] + 1;
+                arr[n - check_size] = arr[n - 1 - check_size] + 1;
                 check_size--;
             }
         }
@@ -97,13 +187,9 @@ void ft_print_combn(int n)
         {
             arr[n - 1]++;
         }
-    // print
-    for(int i = 0; i < n; i++)
-    {
-        ft_putchar(arr[i]);
-    }
-        ft_putchar(',');
-        ft_putchar(' ');
+
+        // print
+        print_num(arr, n);
     }
 }
 
