c2a3224d968b626c5b2f3defb03739d895b5bc53
MingZi <1127abj@gmail.com>
2021-10-18 14:30:06 +0900

    c02 edit 2

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,37 +6,60 @@
 /*   By: byan <byan@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 18:06:18 by byan              #+#    #+#             */
-/*   Updated: 2021/10/18 14:13:33 by byan             ###   ########seoul.kr  */
+/*   Updated: 2021/10/18 14:29:58 by byan             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+int	ft_check_alphabet(char *str, int i)
+{
+	if (str[i] >= 'a' && str[i] <= 'z')
+		return (1);
+	else if (str[i] >= 'A' && str[i] <= 'Z')
+		return (2);
+	else
+		return (0);
+}
+
+int	ft_capitalizer(char *str, int i)
+{
+	if (ft_check_alphabet(str, i) == 1)
+		str[i] = str[i] - 32;
+	i++;
+	while (ft_check_alphabet(str, i) && str[i] != '\0')
+	{
+		if (ft_check_alphabet(str, i) == 2)
+			str[i] = str[i] + 32;
+		i++;
+	}
+	return (i);
+}
+
+int	ft_strlowcase(char *str, int i)
+{
+	while (ft_check_alphabet(str, i) && str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+		i++;
+	}
+	return (i);
+}
 
 char	*ft_strcapitalize(char *str)
 {
-	int		i;
-	char	a;
+	int	i;
 
-	i = 1;
-	if (str[0] >= 'a' && str[0] <= 'z')
-		str[0] = str[0] - 32;
+	i = 0;
+	if (ft_check_alphabet(str, 0) == 1)
+		i = ft_capitalizer(str, i);
 	while (str[i] != '\0')
 	{
-		a = str[i - 1];
-		if (!((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z')))
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				if (!(str[i - 1] >= '0' && str[i - 1] <= '9'))
-					str[i] = str[i] - 32;
-		}
-		if (str[i] >= 'A' && str[i] <= 'Z')
-			if (str[i - 1] >= '0' && str[i - 1] <= '9')
-				str[i] = str[i] + 32;
-		if ((a >= 'A' && a <= 'Z') && (str[i] >= 'A' && str[i] <= 'Z'))
-			str[i] = str[i] + 32;
-		if ((a >= 'a' && a <= 'z') && (str[i] >= 'A' && str[i] <= 'Z'))
-			str[i] = str[i] + 32;
-		i++;
+		while (!(ft_check_alphabet(str, i)) && str[i] != '\0')
+			i++;
+		if (str[i - 1] >= '0' && str[i - 1] <= '9')
+			i = ft_strlowcase(str, i);
+		else if (ft_check_alphabet(str, i))
+			i = ft_capitalizer(str, i);
 	}
 	return (str);
 }
